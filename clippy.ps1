a0c82da561b7e5882901321115bc250e3b57230d
BuyMyMojo <hello@buymymojo.net>
2022-10-28 21:21:24 +1100

    Add my basic clippy presets

---
+++ clippy.ps1
@@ -0,0 +1,1 @@
+cargo +nightly clippy --all-features -Z unstable-options -- -D clippy::correctness -W clippy::style -W clippy::complexity -W clippy::perf -W clippy::nursery -W clippy::pedantic -W clippy::cargo -A clippy::too_many_lines -A clippy::multiple_crate_versions
